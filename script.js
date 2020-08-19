7e5c710a8340f9deaedf7dac46e84020281d0bf4
prajapati-17 <67316197+prajapati-17@users.noreply.github.com>
2020-08-19 15:59:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,17 @@
 Hello Yaakov
 Good Bye John
 Good Bye Jen
-Good Bye Dhoni
-Hello Hitesh
-Hello Praveen
-Hello kohli
-Hello Raina
-Hello Bhuvan
-Good Bye Raj
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
 */
 
 
-var names = ["Yaakov", "John", "Jen", "Dhoni", "Hitesh", "Praveen", "kohli", "Raina", "Bhuvan", "Raj"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i in names) {
 
